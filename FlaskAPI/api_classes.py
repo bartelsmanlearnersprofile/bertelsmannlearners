80c0cd0f843a48a619cf0b077090976094d70841
udoyen <datameshprojects@gmail.com>
2020-12-27 21:06:11 +0100

    added input validation to the api resource classes.

--- FlaskAPI/api_classes.py
+++ FlaskAPI/api_classes.py
@@ -7,6 +7,13 @@ auth = HTTPBasicAuth()
 class LearnerAPI(Resource):
     method_decorators = [auth.login_required]
 
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('slackname', type=str, default='', required=False)
+        self.reqparse.add_argument('firstname', type=str, default='')
+        self.reqparse.add_argument('lastname', type=str, default='')
+        super(LearnerAPI, self).__init__()
+
     def get(self, slackname):
         pass
 
@@ -19,6 +26,15 @@ class LearnerAPI(Resource):
 
 class LearnerListAPI(Resource):
     method_decorators = [auth.login_required]
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('slackname', type=str, required=True, help='No learner slackname is provided.',
+                                   location='json')
+        self.reqparse.add_argument('firstname', type=str, required=True, help='No first name provided.',
+                                   location='json')
+        self.reqparse.add_argument('lastname', type=str, required=True, help='No last name provided.', location='json')
+        super(LearnerListAPI, self).__init__()
 
     def get(self):
         pass
